7e91fafe87fe0b92b659b7559b0f4c7fe73fc349
Densus100 <daniel_goodnezz_5000@yahoo.com>
2018-06-14 21:08:16 +0700

    Menambahkan kotak dan Index
    Jika Kotak di klik bisa menujukkan kotak index ke berapa

--- SFML Sudoku/SFML Sudoku/source.cpp
+++ SFML Sudoku/SFML Sudoku/source.cpp
@@ -4,10 +4,59 @@
 
 using namespace std;
 
+class sudoku{
+protected:
+	sf::RectangleShape kotak[81];
+	sf::FloatRect kotakf[81];
+	int count;
+public:
+	sudoku(){
+		for(int x = 0; x<81; x++){
+			kotak[x].setSize(sf::Vector2f(70,70));
+			kotak[x].setOutlineColor(sf::Color::Red);
+			kotak[x].setOutlineThickness(2);
+		}
+		count = 81;
+	}
+
+	void setposition(){
+		float horisontal = 30, vertical = 30;
+		int co = 0;
+		for(int x = 0; x<81; x++){
+			if(co == 9){
+				horisontal = 30;
+				vertical +=70;
+				co = 0;
+			}
+			kotak[x].setPosition(sf::Vector2f(horisontal, vertical));
+			horisontal+=70;
+			co++;
+		}
+	}
+
+	sf::RectangleShape gambar(int i){
+		return kotak[i];
+	}
+
+	void setrectangle(){
+		for(int x = 0; x<81; x++){
+			kotakf[x] = kotak[x].getGlobalBounds();
+		}
+	}
+
+	int getindexkotak(sf::Vector2f(localPosition)){
+		for(int x = 0; x<81; x++){
+			if(kotakf[x].contains(sf::Vector2f(localPosition))){
+				return x;
+			}
+		}
+	}
+};
+
 void main(){
     sf::RenderWindow window;
 	sf::Style::Close;
-    window.create(sf::VideoMode(800, 600), "My window");
+    window.create(sf::VideoMode(900, 800), "My window");
 	window.setFramerateLimit(60);
 	sf::Font huruf;
 	if (!huruf.loadFromFile("Fonts/Roboto-Regular.ttf")){
@@ -32,8 +81,36 @@ void main(){
 	// inside the main loop, between window.clear() and window.display()
 
 
-	while (window.isOpen())
-    {
+	sf::RectangleShape background;
+	background.setPosition(sf::Vector2f(30,30));
+	background.setSize(sf::Vector2f(630,630));
+	background.setOutlineThickness(5);
+	background.setOutlineColor(sf::Color::Black);
+
+	sudoku kotak;
+	kotak.setposition();
+	kotak.setrectangle();
+
+	sf::FloatRect backgroundf;
+	backgroundf = background.getGlobalBounds();
+
+
+	while (window.isOpen()){
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+			sf::Vector2i localPosition = sf::Mouse::getPosition(window);
+
+			int poslx = localPosition.x;
+			int posly = localPosition.y;
+			cout<<"Local Position = "<<poslx<<", "<<posly<<endl;
+
+			if(backgroundf.contains(sf::Vector2f(localPosition))){
+				cout<<"Mouse In the Box\n";
+				int x = kotak.getindexkotak(sf::Vector2f(localPosition));
+				cout<<"Index Kotak Ke - "<<x<<endl;
+			}
+
+		}
+
         // check all the window's events that were triggered since the last iteration of the loop
         sf::Event event;
         while (window.pollEvent(event))
@@ -43,7 +120,11 @@ void main(){
                 window.close();
         }
 		window.clear(sf::Color::White);
+		window.draw(background);
 		window.draw(tulisan);
+		for(int x = 0; x<81; x++){
+			window.draw(kotak.gambar(x));
+		}
 		window.display();
     }
 
